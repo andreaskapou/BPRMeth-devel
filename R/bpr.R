78492ffb63577e4a8e5510ffbc93d5d7e9390a23
andreaskapou <kapouranis.andreas@gmail.com>
2017-01-25 11:36:16 +0000

    Keep old functions, now we have equivalent in C++

--- R/bpr.R
+++ R/bpr.R
@@ -1,3 +1,49 @@
+#' @name bpr_model
+#' @rdname bpr_model
+#' @aliases bpr
+#'
+#' @title Compute the BPR functions
+#'
+#' @description These functions evaluate the BPR model likelihood and gradient
+#'   using both the Bernoulli and Binomial likelihoods. There are also functions
+#'   to compute the sum of BPR likelihoods and weighted sum of BPR likelihoods.
+#'   They are written in C++ for efficiency.
+#' @section Mathematical formula: The Binomial distributed Probit Regression log
+#'   likelihood function is computed by the following formula: \deqn{log p(y |
+#'   f, w) = \sum_{l=1}^{L} log Binom(m_{l} | t_{l}, \Phi(w^{t}h(x_{l})))} where
+#'   h(x_{l}) are the basis functions.
+#'
+#' @param w A vector of parameters (i.e. coefficients of the basis functions)
+#' @param H The \code{L x M} matrix design matrix, where L is the number of
+#'   observations and M the number of basis functions.
+#' @param data Either an \code{L x 3} matrix containing in the 1st column are
+#'   the observations, in the 2nd column the total number of trials and in the
+#'   3rd the number of successes or an \code{L x 2} matrix containing the
+#'   Bernoulli observations in the 2nd column. Each row corresponds to each row
+#'   of the design matrix.
+#' @param x A list of elements of length N, where each element is an L x 3
+#'   matrix of observations, where 1st column contains the locations. The 2nd
+#'   and 3rd columns contain the total trials and number of successes at the
+#'   corresponding locations, repsectively. If we have Bernoulli data, then the
+#'   matrix is L x 2, where the 2nd column contiains the Bernoulli observations.
+#' @param des_mat A list of length N, where each element contains the \code{L x
+#'   M} design matrices, where L is the number of observations and M the number
+#'   of basis functions.
+#' @param pi_k Mixing proportions in log scale.
+#' @param post_prob A vector of length N containing the posterior probabilities
+#'   for each element of list x, respectively.
+#' @param lambda The complexity penalty coefficient for penalized regression.
+#' @param is_NLL Logical, indicating if the Negative Log Likelihood should be
+#'   returned.
+#'
+#' @return Either the BPR log likelihood or the gradient.
+#'
+#' @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
+#'
+#' @seealso \code{\link{eval_functions}}, \code{\link{bpr_optimize}}
+NULL
+
+
 # (Internal) BPR log likelihood function
 #
 # \code{bpr_likelihood} evaluates the Binomial distributed Probit regression log
@@ -27,7 +73,7 @@
 #
 # @importFrom stats pnorm dbinom
 #
-.bpr_likelihood <- function(w, H, data, lambda = 1/2, is_NLL = FALSE){
+.old_bpr_likelihood <- function(w, H, data, lambda = 1/2, is_NLL = FALSE){
     # If the data is matrix, we have Binomial observations
     if (is.matrix(data)){
         total <- data[, 1]
@@ -78,9 +124,15 @@
 #
 # @importFrom stats pnorm dnorm
 #
-.bpr_gradient <- function(w, H, data, lambda = 1/2, is_NLL = FALSE){
-    total <- data[, 1]
-    succ  <- data[, 2]
+.old_bpr_gradient <- function(w, H, data, lambda = 1/2, is_NLL = FALSE){
+    # If the data is matrix, we have Binomial observations
+    if (is.matrix(data)){
+        total <- data[, 1]
+        succ  <- data[, 2]
+    }else{ # ... otherwise we have Bernoulli observations.
+        succ <- data
+        total <- rep(1, length(succ))
+    }
 
     # Predictions of the target variables
     g <- as.vector(H %*% w)
@@ -141,23 +193,15 @@
 .sum_weighted_bpr_lik <- function(w, x, des_mat, post_prob, lambda = 1/2,
                                   is_NLL = TRUE){
     N <- length(x)
-    
-    # Cases when we have Binomial or Bernoulli data
-    if (NCOL(x[[1]]) == 3){
-      # Methylation data
-      cols <- 2:3
-    }else{
-      cols <- 2
-    }
-    
+
     # TODO: Create tests
     # For each element in x, evaluate the BPR log likelihood
     res <- vapply(X   = 1:N,
-                  FUN = function(y) .bpr_likelihood(w = w,
-                                                    H = des_mat[[y]]$H,
-                                                    data = x[[y]][, cols],
-                                                    lambda = lambda,
-                                                    is_NLL = is_NLL),
+                  FUN = function(y) .old_bpr_likelihood(w = w,
+                                                   H = des_mat[[y]]$H,
+                                                   data = x[[y]][,2:3],
+                                                   lambda = lambda,
+                                                   is_NLL = is_NLL),
                   FUN.VALUE = numeric(1),
                   USE.NAMES = FALSE)
 
@@ -185,22 +229,14 @@
                                    is_NLL = TRUE){
     N <- length(x)
 
-    # Cases when we have Binomial or Bernoulli data
-    if (NCOL(x[[1]]) == 3){
-      # Methylation data
-      cols <- 2:3
-    }else{
-      cols <- 2
-    }
-    
     # TODO: Create tests
     # For each element in x, evaluate the gradient of the BPR log likelihood
     res <- vapply(X   = 1:N,
-                  FUN = function(y) .bpr_gradient(w = w,
-                                                  H = des_mat[[y]]$H,
-                                                  data = x[[y]][, cols],
-                                                  lambda = lambda,
-                                                  is_NLL = is_NLL),
+                  FUN = function(y) .old_bpr_gradient(w = w,
+                                                 H = des_mat[[y]]$H,
+                                                 data = x[[y]][,2:3],
+                                                 lambda = lambda,
+                                                 is_NLL = is_NLL),
                   FUN.VALUE = numeric(length(w)),
                   USE.NAMES = FALSE)
 
