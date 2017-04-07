fe998f37fc549684455c22e7defe5efb29ef928a
andreaskapou <kapouranis.andreas@gmail.com>
2017-04-07 18:23:24 +0100

    Faster implementation of BPR-FDMM model

--- R/sc_bayes_bpr_fdmm.R
+++ R/sc_bayes_bpr_fdmm.R
@@ -35,20 +35,15 @@
 #' @importFrom mvtnorm rmvnorm
 #' @importFrom utils txtProgressBar setTxtProgressBar
 #' @export
-sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
-                              basis = NULL, w_0_mean = NULL, w_0_cov = NULL,
-                              dir_a = rep(1/K, K), lambda = 1/2,
-                              gibbs_nsim = 5000, gibbs_burn_in = 1000,
-                              inner_gibbs = FALSE, gibbs_inner_nsim = 50,
-                              is_parallel = TRUE, no_cores = NULL,
-                              is_verbose = TRUE){
+sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL, basis = NULL, w_0_mean = NULL, w_0_cov = NULL,
+                              dir_a = rep(1/K, K), lambda = 1/2, gibbs_nsim = 5000, gibbs_burn_in = 1000,
+                              inner_gibbs = FALSE, gibbs_inner_nsim = 50, is_parallel = TRUE, no_cores = NULL, is_verbose = TRUE){
 
     # Check that x is a list object
     assertthat::assert_that(is.list(x))
     assertthat::assert_that(is.list(x[[1]]))
 
-    # If parallel mode is ON
-    if (is_parallel){
+    if (is_parallel){ # If parallel mode is ON
         # If number of cores is not given
         if (is.null(no_cores)){ no_cores <- parallel::detectCores() - 2
         }else{
@@ -61,82 +56,60 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
         doParallel::registerDoParallel(cl)
     }
 
-    # Extract number of cells
-    I <- length(x)
-
-    # TODO: Shall we consider different number of promoters for each cell?
-    # Extract number of promoter regions
-    N <- length(x[[1]])
-
-    # Number of coefficients for modelling each promoter methylation profile
-    M <- basis$M + 1
+    I <- length(x)      # Extract number of cells
+    N <- length(x[[1]]) # Extract number of promoter regions
+    if (is.null(basis)){ basis <- create_rbf_object(M = 3) }
+    M <- basis$M + 1    # Number of coefficient parameters
 
     # Initialize priors over the parameters
-    if (is.null(w_0_mean)){
-        w_0_mean <- rep(0, M)
-    }
-    if (is.null(w_0_cov)){
-        w_0_cov <- diag(2, M)
-    }
-    # Invert covariance matrix to get the precision matrix
-    prec_0 <- solve(w_0_cov)
-    # Compute product of prior mean and prior precision matrix
-    w_0_prec_0 <- prec_0 %*% w_0_mean
+    if (is.null(w_0_mean)){ w_0_mean <- rep(0, M) }
+    if (is.null(w_0_cov)){ w_0_cov <- diag(2, M) }
 
-    # Matrices for storing results
+    prec_0 <- solve(w_0_cov)          # Invert covariance matrix to get the precision matrix
+    w_0_prec_0 <- prec_0 %*% w_0_mean # Compute product of prior mean and prior precision matrix
+
+    # Matrices / Lists for storing results
     w_pdf        <- matrix(0, nrow = I, ncol = K)  # Store weighted PDFs
     post_prob    <- matrix(0, nrow = I, ncol = K)  # Hold responsibilities
-    C_i          <- matrix(0, nrow = I, ncol = K)  # Mixture components
-    C_i_prev     <- C_i # Keep previous mixture components
-    C_matrix     <- matrix(0, nrow = I, ncol = K)  # Total Mixture components
+    C            <- matrix(0, nrow = I, ncol = K)  # Mixture components
+    C_prev       <- C                              # Keep previous components
+    C_matrix     <- matrix(0, nrow = I, ncol = K)  # Total mixture components
     NLL          <- vector(mode = "numeric", length = gibbs_nsim)
     NLL[1]       <- 1e+100
 
-    H <- list()
-    y <- list()
-    z <- list()
-    V <- list()
-    len_y <- matrix(0, nrow = K, ncol = N)
-    sum_y <- matrix(0, nrow = K, ncol = N)
+    H = y = z = V <- list()
     for (k in 1:K){
-        H[[k]] <- vector("list", N)
-        y[[k]] <- vector("list", N)
-        z[[k]] <- vector("list", N)
-        V[[k]] <- vector("list", N)
+        H[[k]] <- vector("list", N) # List of concatenated design matrices
+        y[[k]] <- vector("list", N) # List of observed methylation data
+        z[[k]] <- vector("list", N) # List of auxiliary latent variables
+        V[[k]] <- vector("list", N) # List of posterior variances
     }
+    len_y <- matrix(0, nrow = K, ncol = N) # Total CpG observations per region
+    sum_y <- matrix(0, nrow = K, ncol = N) # Total methylated CpGs per region
 
     # Store mixing proportions draws
     pi_draws <- matrix(NA_real_, nrow = gibbs_nsim, ncol = K)
     pi_draws[1, ] <- pi_k
 
     # Store BPR coefficient draws
-    w_draws <- vector(mode = "list", length = gibbs_nsim)
+    w_draws <- array(data = 0, dim = c(gibbs_nsim, N, M , K))
     # TODO: Initialize w in a sensible way
-    if (is.null(w)){
-        w <- array(data = 0, dim = c(N, M, K))
-        #for (k in 2:K){
-        #  w[, , k] <- k * 0.1
-        #}
-    }
-    w_draws[[1]] <- w
+    if (is.null(w)){ w <- array(data = 0, dim = c(N, M, K)) }
+    w_draws[1, , ,] <- w
 
-    # Kee a list of promoter regions with CpG coverage
-    ind <- list()
-    # Create design matrix for each cell for each promoter region
-    des_mat <- list()
+    ind <- list()     # Keep a list of promoter regions with CpG coverage
+    des_mat <- list() # Create design matrix for each cell for each promoter region
     for (i in 1:I){
         des_mat[[i]] <- vector(mode = "list", length = N)
-        ind[[i]] <- which(!is.na(x[[i]]))
-        # TODO: Make this function faster?
-        if (is_parallel){
-            des_mat[[i]][ind[[i]]] <- parallel::mclapply(X = x[[i]][ind[[i]]], FUN = function(y) .design_matrix(x = basis, obs = y[, 1])$H, mc.cores = no_cores)
+        ind[[i]]     <- which(!is.na(x[[i]]))
+        if (is_parallel){  # TODO: Make this function faster?
+            des_mat[[i]][ind[[i]]] <- parallel::mclapply(X = x[[i]][ind[[i]]], FUN = function(y)
+                design_matrix(x = basis, obs = y[, 1])$H, mc.cores = no_cores)
         }else{
-            des_mat[[i]][ind[[i]]] <- lapply(X = x[[i]][ind[[i]]], FUN = function(y) .design_matrix(x = basis, obs = y[, 1])$H)
+            des_mat[[i]][ind[[i]]] <- lapply(X = x[[i]][ind[[i]]], FUN = function(y) design_matrix(x = basis, obs = y[, 1])$H)
         }
-        ##des_mat[[i]][-ind[[i]]] <- NA
     }
-    if (is_parallel){
-        # Stop parallel execution
+    if (is_parallel){ # Stop parallel execution
         parallel::stopCluster(cl)
         doParallel::stopImplicitCluster()
     }
@@ -151,68 +124,52 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
         ## ---------------------------------------------------------------
         # Compute weighted pdfs for each cluster
         for (k in 1:K){
-            # Iterate over each cell
-            for (i in 1:I){
-                # Apply only to regions with CpG coverage
-                w_pdf[i, k] <- sum(vapply(X = ind[[i]], FUN = function(y) bpr_likelihood(w = w[y, , k], H = des_mat[[i]][[y]],
-                                                                                         data = x[[i]][[y]], lambda = lambda, is_NLL = FALSE),
-                                          FUN.VALUE = numeric(1), USE.NAMES = FALSE))
-                # TODO: Do we need to do anything with regions with no CpGs??
-            }
-            w_pdf[, k] <- log(pi_k[k]) + w_pdf[, k]
+            # Apply to each cell and only to regions with CpG coverage
+            w_pdf[, k] <- log(pi_k[k]) + vapply(X = 1:I, FUN = function(i) sum(vapply(X = ind[[i]], FUN = function(y)
+                bpr_likelihood(w = w[y, , k], H = des_mat[[i]][[y]], data = x[[i]][[y]], lambda = lambda, is_NLL = FALSE),
+                FUN.VALUE = numeric(1), USE.NAMES = FALSE)), FUN.VALUE = numeric(1), USE.NAMES = FALSE)
         }
         # Use the logSumExp trick for numerical stability
         Z <- apply(w_pdf, 1, .log_sum_exp)
         # Get actual posterior probabilities, i.e. responsibilities
         post_prob <- exp(w_pdf - Z)
-        # Evaluate and store the NLL
-        NLL[t] <- -sum(Z)
+        NLL[t] <- -sum(Z)   # Evaluate NLL
 
         ## -------------------------------------------------------------------
         # Draw mixture components for ith simulation
-        for (i in 1:I){ # Sample one point from a Multinomial i.e. ~ Discrete
-            C_i[i, ] <- rmultinom(n = 1, size = 1, post_prob[i, ])
-        }
+        # Sample one point from a Multinomial i.e. ~ Discrete
+        for (i in 1:I){ C[i, ] <- rmultinom(n = 1, size = 1, post_prob[i, ]) }
         # TODO: Should we keep all data
-        if (t > gibbs_burn_in){
-            C_matrix <- C_matrix + C_i
-        }
+        if (t > gibbs_burn_in){ C_matrix <- C_matrix + C }
 
         ## -------------------------------------------------------------------
-        # Calculate component counts of each cluster
-        N_k <- colSums(C_i)
-        # Update mixing proportions using new cluster component counts
-        pi_k <- as.vector(rdirichlet(n = 1, alpha = dir_a + N_k))
+        # Update mixing proportions using updated cluster component counts
+        pi_k <- as.vector(rdirichlet(n = 1, alpha = dir_a + colSums(C)))
         pi_draws[t, ] <- pi_k
 
         # Matrix to keep promoters with no CpG coverage
         empty_region <- matrix(0, nrow = N, ncol = K)
         for (k in 1:K){
             # Which cells are assigned to cluster k
-            C_k_idx <- which(C_i[, k] == 1)
-            # TODO: Handle cases when clusters are empty!!
-            if (length(C_k_idx) == 0){
-                message("Warning: Empty cluster...")
-                next # TODO: How to handle empty clusters
-            }
-
+            C_idx <- which(C[, k] == 1)
+            # TODO: Handle cases when we have empty clusters...
+            if (length(C_idx) == 0){ message("Warning: Empty cluster..."); next }
             # Check if current clusters ids are not equal to previous ones
-            if (!identical(C_i[, k], C_i_prev[, k])){
+            if (!identical(C[, k], C_prev[, k])){
                 message(t, ": Not identical")
                 # Iterate over each promoter region
                 for (n in 1:N){
                     # Initialize empty vector for observed methylation data
                     yy <- vector(mode = "integer")
                     # Concatenate the nth promoter from all cells in cluster k
-                    H[[k]][[n]] <- do.call(rbind, lapply(des_mat, "[[", n)[C_k_idx])
+                    H[[k]][[n]] <- do.call(rbind, lapply(des_mat, "[[", n)[C_idx])
 
                     # TODO: Check when we have empty promoters....
-                    if (is.null(H[[k]][[n]])){
-                        empty_region[n, k] <- 1
-                    }else{
+                    if (is.null(H[[k]][[n]])){ empty_region[n, k] <- 1 }
+                    else{
                         # Obtain the corresponding methylation levels
-                        for (cell in seq_along(C_k_idx)){
-                            obs <- x[[C_k_idx[cell]]][[n]]
+                        for (cell in seq_along(C_idx)){
+                            obs <- x[[C_idx[cell]]][[n]]
                             if (length(obs) > 1){ yy <- c(yy, obs[, 2]) }
                         }
                         # Precompute for faster computations
@@ -220,7 +177,6 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
                         sum_y[k, n] <- sum(yy)
                         y[[k]][[n]] <- yy
                         z[[k]][[n]] <- rep(NA_real_, len_y[k, n])
-
                         # Compute posterior variance of w_nk
                         V[[k]][[n]] <- solve(prec_0 + crossprod(H[[k]][[n]], H[[k]][[n]]))
                     }
@@ -229,10 +185,8 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
 
             for (n in 1:N){
                 # In case we have no CpG data in this promoter
-                if (is.null(H[[k]][[n]])){
-                    next
-                }
-                # Perform Gibbs to sample from the augmented BPR model
+                if (is.null(H[[k]][[n]])){ next }
+                # Perform Gibbs sampling on the augmented BPR model
                 if (inner_gibbs){
                     w_inner <- matrix(0, nrow = gibbs_inner_nsim, ncol = M)
                     w_inner[1, ] <- w[n, , k]
@@ -251,17 +205,11 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
                         # Compute posterior mean of w
                         Mu <- V[[k]][[n]] %*% (w_0_prec_0 + crossprod(H[[k]][[n]], z[[k]][[n]]))
                         # Draw variable \w from its full conditional: \w | z, X
-                        if (M == 1){
-                            w_inner[tt, ] <- c(rnorm(n = 1, mean = Mu, sd = V[[k]][[n]]))
-                        }else{
-                            w_inner[tt, ] <- c(rmvnorm(n = 1, mean = Mu, sigma = V[[k]][[n]]))
-                        }
+                        if (M == 1){ w_inner[tt, ] <- c(rnorm(n = 1, mean = Mu, sd = V[[k]][[n]])) }
+                        else{ w_inner[tt, ] <- c(rmvnorm(n = 1, mean = Mu, sigma = V[[k]][[n]])) }
                     }
-                    if (M == 1){
-                        w[n, , k] <- mean(w_inner[-(1:(gibbs_inner_nsim/2)), ])
-                    }else{
-                        w[n, , k] <- colMeans(w_inner[-(1:(gibbs_inner_nsim/2)), ])
-                    }
+                    if (M == 1){ w[n, , k] <- mean(w_inner[-(1:(gibbs_inner_nsim/2)), ]) }
+                    else{ w[n, , k] <- colMeans(w_inner[-(1:(gibbs_inner_nsim/2)), ]) }
                 }else{
                     # Update Mean of z
                     mu_z <- H[[k]][[n]] %*% w[n, , k]
@@ -277,11 +225,8 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
                     # Compute posterior mean of w
                     Mu <- V[[k]][[n]] %*% (w_0_prec_0 + crossprod(H[[k]][[n]], z[[k]][[n]]))
                     # Draw variable \w from its full conditional: \w | z, X
-                    if (M == 1){
-                        w[n, , k] <- c(rnorm(n = 1, mean = Mu, sd = V[[k]][[n]]))
-                    }else{
-                        w[n, , k] <- c(rmvnorm(n = 1, mean = Mu, sigma = V[[k]][[n]]))
-                    }
+                    if (M == 1){ w[n, , k] <- c(rnorm(n = 1, mean = Mu, sd = V[[k]][[n]])) }
+                    else{ w[n, , k] <- c(rmvnorm(n = 1, mean = Mu, sigma = V[[k]][[n]])) }
                 }
             }
         }
@@ -291,15 +236,11 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
         for (n in 1:N){
             clust_empty_ind <- which(empty_region[n, ] == 1)
             # No empty promoter regions
-            if (length(clust_empty_ind) == 0){
-                next
-            } # Case that should never happen with the right preprocessing step
+            if (length(clust_empty_ind) == 0){ next }
+            # Case that should never happen with the right preprocessing step
             else if (length(clust_empty_ind) == K){
-                for (k in 1:K){
-                    w[n, , k] <- c(rmvnorm(1, w_0_mean, w_0_cov))
-                }
+                for (k in 1:K){ w[n, , k] <- c(rmvnorm(1, w_0_mean, w_0_cov)) }
             }else{
-                # message("Promoter: ", n)
                 # TODO: Perform a better imputation approach...
                 cover_ind <- which(empty_region[n, ] == 0)
                 # Randomly choose a cluster to obtain the methylation profiles
@@ -309,68 +250,31 @@ sc_bayes_bpr_fdmm <- function(x, K = 2, pi_k = rep(1/K, K), w = NULL,
                 }
             }
         }
-        # Make current cluster indices same as previous
-        C_i_prev <- C_i
-        # Store the coefficient draw
-        w_draws[[t]] <- w
-
+        C_prev <- C            # Make current cluster indices same as previous
+        w_draws[t, , ,] <- w   # Store the coefficient draw
         setTxtProgressBar(pb,t)
     }
     close(pb)
     message("Finished Gibbs sampling...")
 
-
     ##-----------------------------------------------
     message("Computing summary statistics...")
     # Compute summary statistics from Gibbs simulation
-    if (K == 1){
-        pi_post <- mean(pi_draws[gibbs_burn_in:gibbs_nsim, ])
-    }else{
-        pi_post <- colMeans(pi_draws[gibbs_burn_in:gibbs_nsim, ])
-    }
+    if (K == 1){ pi_post <- mean(pi_draws[gibbs_burn_in:gibbs_nsim, ]) }
+    else{ pi_post <- colMeans(pi_draws[gibbs_burn_in:gibbs_nsim, ]) }
     C_post <- C_matrix / (gibbs_nsim - gibbs_burn_in)
     w_post <- array(0, dim = c(N, M, K))
-    for (n in 1:N){
-        for (k in 1:K){
-            w <- rep(0, M)
-            for (t in gibbs_burn_in:gibbs_nsim){
-                w <- w + w_draws[[t]][n, ,k]
-            }
-            w_post[n, , k] <- w / (gibbs_nsim - gibbs_burn_in)
-        }
-    }
+    for(k in 1:K){ w_post[, , k] <- colSums(w_draws[gibbs_burn_in:gibbs_nsim, , , k]) / (gibbs_nsim - gibbs_burn_in)  }
 
     # Object to keep input data
-    dat <- NULL
-    dat$K <- K
-    dat$N <- N
-    dat$I <- I
-    dat$M <- M
-    dat$basis <- basis
-    dat$dir_a <- dir_a
-    dat$lambda <- lambda
-    dat$w_0_mean <- w_0_mean
-    dat$w_0_cov <- w_0_cov
-    dat$gibbs_nsim <- gibbs_nsim
-    dat$gibbs_burn_in <- gibbs_burn_in
-
+    dat <- list(K = K, N = N, I = I, M = M, basis = basis, dir_a = dir_a, lambda = lambda,
+                w_0_mean = w_0_mean, w_0_cov = w_0_cov, gibbs_nsim = gibbs_nsim, gibbs_burn_in = gibbs_burn_in)
     # Object to hold all the Gibbs draws
-    draws <- NULL
-    draws$pi <- pi_draws
-    draws$w <- w_draws
-    draws$C <- C_matrix
-    draws$NLL <- NLL
-
+    draws <- list(pi = pi_draws, w = w_draws, C = C_matrix, NLL = NLL)
     # Object to hold the summaries for the parameters
-    summary <- NULL
-    summary$w <- w_post
-    summary$pi <- pi_post
-    summary$C <- C_post
-
+    summary <- list(pi = pi_post, w = w_post, C = C_post)
     # Create sc_bayes_bpr_fdmm object
-    obj <- structure(list(summary = summary,
-                          draws   = draws,
-                          dat     = dat),
+    obj <- structure(list(summary = summary, draws = draws, dat = dat),
                      class = "sc_bayes_bpr_fdmm")
     return(obj)
 }
